81c6c74be2e2dbb144b9d7ab42a7ae10699652bd
Michael Ball <michael.h.s.ball@gmail.com>
2022-06-22 23:00:15 +0200

    started adding CLI definition

---
+++ redband/cli.py
@@ -0,0 +1,69 @@
+import argparse
+
+
+def get_args_parser() -> argparse.ArgumentParser:
+    from . import __version__
+
+    # TODO: Can I use CLICK here instead of ArgumentParser ??
+
+    parser = argparse.ArgumentParser(add_help=False, description="RedBand")
+    parser.add_argument("--help", "-h", action="store_true", help="Application's help")
+    parser.add_argument("--redband-help", action="store_true", help="RedBand's help")
+    parser.add_argument(
+        "--version",
+        action="version",
+        help="Show RedBand's version and exit",
+        version=f"RedBand {__version__}",
+    )
+    parser.add_argument(
+        "overrides",
+        nargs="*",
+        help="Any key=value arguments to override config values (use dots for.nested=overrides)",
+    )
+
+    parser.add_argument(
+        "--show",
+        "-s",
+        action="store_true",
+        help="Display config instead of running entrypoint",
+    )
+
+    parser.add_argument(
+        "--config-path",
+        "-cp",
+        help=(
+            "Overrides the config_path specified in redband.entrypoint(). "
+            "The `config_path` can either be absolute or relative to the `.py` file containing @redband.entrypoint()"
+        ),
+    )
+
+    parser.add_argument(
+        "--config-name",
+        "-cn",
+        help="Overrides the `config_name` specified in @redband.entrypoint()",
+    )
+
+    parser.add_argument(
+        "--config-dir",
+        "-cd",
+        help="Adds an additional config dir to the config search path",
+    )
+
+    parser.add_argument(
+        "--config",
+        "-c",
+        help=(
+            "Runs the entrypoint with a serialized config object, bypassing all config composition "
+            "(all other command-line arguments are ignored)"
+        ),
+    )
+
+    # TODO: add this back later
+    # parser.add_argument(
+    #     "--multirun",
+    #     "-m",
+    #     action="store_true",
+    #     help="Run multiple jobs with the configured launcher and sweeper",
+    # )
+
+    return parser
